a962c244536b043f97164baed32cfd52a8c176f2
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-11-26 16:38:40 +0000

    Better distro selection

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo $1
-docker build --build-arg INTERFACE=$1 -t ros2:foxy-go2 -f Dockerfile . 
+
+if [ -z "$2" ]; then
+    docker build --build-arg INTERFACE=$1 -t ros2:foxy-go2 -f Dockerfile .
+else
+    docker build --build-arg INTERFACE=$1 --build-arg ROS_DISTRO=$2 -t ros2:foxy-go2 -f Dockerfile .
+fi
