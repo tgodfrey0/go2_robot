cc9f1cf85dd24842b66a41f0759ee366689391a6
Toby Godfrey <toby.godfrey2003@gmail.com>
2025-01-09 12:23:29 +0000

    disable auto sourcing of ws for testing

--- auxiliary/Docker/_ros2_rmw_setup.sh
+++ auxiliary/Docker/_ros2_rmw_setup.sh
@@ -4,4 +4,5 @@ source /opt/ros/${ROS_DISTRO}/setup.bash
 source /cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="${INTERFACE}" priority="default" multicast="default" /></Interfaces></General></Domain></CycloneDDS>'
-source /go2_ws/install/setup.bash
+# source /go2_ws/install/setup.bash
+echo "Sourcing of go2_ws is DISABLED"
