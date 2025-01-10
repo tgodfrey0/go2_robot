75e9aa7f1f27f5920e615647de22805fa0cda322
Toby Godfrey <tmag1g21@soton.ac.uk>
2025-01-10 16:53:08 +0000

    comment

--- auxiliary/Docker/_ros2_rmw_setup.sh
+++ auxiliary/Docker/_ros2_rmw_setup.sh
@@ -6,3 +6,5 @@ export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="${CYCLONEDDS_INTERFACE}" priority="default" multicast="default" /></Interfaces></General></Domain></CycloneDDS>'
 # source /go2_ws/install/setup.bash
 echo "Sourcing of go2_ws is DISABLED"
+
+#TODO Fix interface substitution
