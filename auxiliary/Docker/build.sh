3e2a78f00393fda8b0c9b5a8e0e367b7b9b1c3e5
T.M.A.Godfrey <tmag1g21@uos-24rjd44.clients.soton.ac.uk>
2025-01-10 12:40:23 +0000

    support podman emulation

--- auxiliary/Docker/build.sh
+++ auxiliary/Docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$2" ]; then
-    docker build --build-arg INTERFACE=$1 -t ros2:foxy-go2 -f Dockerfile .
+    docker build  --build-arg INTERFACE=$1 -t ros2:foxy-go2 -f Dockerfile .
 else
     docker build --build-arg INTERFACE=$1 --build-arg ROS_DISTRO=$2 -t ros2:foxy-go2 -f Dockerfile .
 fi
