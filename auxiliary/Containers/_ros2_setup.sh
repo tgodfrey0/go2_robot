d3ba556f2873aa6551b504dc8948bea94cc5d62d
Toby Godfrey <tmag1g21@soton.ac.uk>
2025-01-20 18:23:14 +0000

    fixed thanks george

--- auxiliary/Containers/_ros2_setup.sh
+++ auxiliary/Containers/_ros2_setup.sh
@@ -3,6 +3,9 @@ echo "This image was build for the interface {CYCLONEDDS_INTERFACE}"
 source /opt/ros/${ROS_DISTRO}/setup.bash # This gets sourced automatically somewhere else
 source /cyclonedds_ws/install/setup.bash # This causes the hanging
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="{CYCLONEDDS_INTERFACE}" priority="default" multicast="default" /></Interfaces></General></Domain></CycloneDDS>'
+export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces><AllowMulticast>false</AllowMulticast><NetworkInterface name="{CYCLONEDDS_INTERFACE}" priority="default" multicast="default" /></Interfaces></General></Domain></CycloneDDS>'
 source /go2_ws/install/setup.bash
 echo "Sourcing of go2_ws is DISABLED"
+
+
+# TODO No mulitcast
